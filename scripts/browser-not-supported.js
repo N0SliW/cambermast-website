811f82543dcea2fbb29022b432068ef32b1bad1c
Bill Raymond <bill.raymond@cambermast.com>
2020-08-21 17:45:56 -0700

    comment out the alert javascript message

--- scripts/browser-not-supported.js
+++ scripts/browser-not-supported.js
@@ -1,7 +1,7 @@
 var version = getIEVersion();
 
 if (version && version <= 15) {
-    alert("This website is in test mode. Message: " + version);
+    // alert("This website is in test mode. Message: " + version);
     document.getElementById("browser-not-supported").style.display = "display: block;";
 }
 

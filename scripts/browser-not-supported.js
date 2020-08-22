e2b3d94435586a9c37f88ef77aa3224627d889bb
Bill Raymond <bill.raymond@cambermast.com>
2020-08-21 17:11:10 -0700

    test that browser-not-supported is functioning correctly

--- scripts/browser-not-supported.js
+++ scripts/browser-not-supported.js
@@ -1,6 +1,7 @@
 var version = getIEVersion();
-
+alert("this website is in test mode. Message: " + version);
 if (version && version <= 15) {
+    
     document.getElementById("browser-not-supported").style.display = "grid";
 }
 
